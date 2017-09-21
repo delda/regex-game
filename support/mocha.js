f056ac34de60f1f2814a1434d16f6bee3bd95050
Davide Dell'Erba <delda@virgilio.it>
2017-09-21 17:33:06 +0200

    Primi test x travis

---
+++ support/mocha.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+var dirname = require.resolve('../node_modules/.bin/workshopper-adventure-test')
+var Mocha = require('mocha')
+var path = require('path')
+var mocha = new Mocha({
+    timeout: 15000
+})
+if (process.argv[2]) {
+    process.chdir(process.argv[2])
+}
+mocha.reporter('spec')
+mocha.files.push(path.join(dirname, '../..', 'spec') + '/exercises.js')
+mocha.files.push(path.join(dirname, '../..', 'spec') + '/info.js')
+mocha.fullTrace()
+mocha.run(process.exit.bind(process))
